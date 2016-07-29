84663a6c9992a2e3994e672b57e702f9146169fc
ryanrdl <ryan_litvak@jabil.com>
2016-07-29 09:35:40 -0400

    removed old projects

--- WebClient/WebClientConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebClient
-{
-    public class WebClientConfiguration
-    {
-        public static string PingUrl { get; set; } = "http://localhost:19701/api/ping";
-        public static string PongUrl { get; set; } = "http://localhost:19702/api/pong";
-    }
-}
