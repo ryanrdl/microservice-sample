1cfb1903cb6466a6f7f77b8f9b808df4d8b46822
ryanrdl <ryan_litvak@jabil.com>
2016-08-01 08:34:55 -0400

    added publish script

---
+++ publish.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [string] $configuration = "Release",
+    [string] $webAppOutDir = ""
+)
+
+& (Resolve-Path "C:\Program Files (x86)\MSBuild\*\Bin\MSBuild.exe") .\WebApp\WebApp.csproj /p:Configuration=$configuration /p:OutDir=$webAppOutDir
+
+dotnet publish .\PingApi -o c:\output\PingApi -c $configuration -r active
+dotnet publish .\PingService -o c:\output\PingService -c $configuration -r active
+dotnet publish .\PongApi -o c:\output\PongApi -c $configuration -r active
+dotnet publish .\PongService -o c:\output\PongService -c $configuration -r active
