cffb17fd028a1786573ac88c6c7fe349dc78530d
ryanrdl <ryan_litvak@jabil.com>
2016-08-01 11:08:45 -0400

    fixed publish script

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,10 @@
 ﻿param(
     [string] $configuration = "Release",
-    [string] $webAppOutDir = ""
+    [string] $webAppPublishUrl = ""
 )
 
-& (Resolve-Path "C:\Program Files (x86)\MSBuild\*\Bin\MSBuild.exe") .\WebApp\WebApp.csproj /p:Configuration=$configuration /p:OutDir=$webAppOutDir
+& (Resolve-Path "C:\Program Files (x86)\MSBuild\*\Bin\MSBuild.exe") .\WebApp\WebApp.csproj /t:WebPublish /p:Configuration=$configuration /p:WebPublishMethod=FileSystem /p:publishUrl=$webAppPublishUrl
+
 
 dotnet publish .\PingApi -o c:\output\PingApi -c $configuration -r active
 dotnet publish .\PingService -o c:\output\PingService -c $configuration -r active
