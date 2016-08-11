d12b9b021bd379a4c5bf37efa130b304df1880cb
ryanrdl <ryan_litvak@jabil.com>
2016-08-11 09:16:28 -0400

    fixed signalr hubs not loading

--- WebApp2/Startup.cs
+++ WebApp2/Startup.cs
@@ -4,15 +4,15 @@ using WebApp2;
 
 [assembly: OwinStartup(typeof(Startup))]
 namespace WebApp2
-{
+{ 
     using Owin;
 
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            app.MapSignalR(); //this has to come before UseNancy or signalr/hubs will not be found on the client
             app.UseNancy();
-            app.MapSignalR();
         }
     }
 }
