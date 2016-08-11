bb4180c51c8563e44ad360c559a57f87203dfde9
ryanrdl <ryan_litvak@jabil.com>
2016-08-11 09:17:27 -0400

    signalr works now that the messagehub is added

---
+++ WebApp2/MessageHub.cs
@@ -0,0 +1,39 @@
+﻿namespace WebApp2
+{
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+    using NServiceBus;
+    using PingMessages.Events;
+
+    public class MessageHandler : //Hub, 
+        IHandleMessages<PingReceived>,
+        IHandleMessages<PingComplete>,
+        IHandleMessages<PingFailed>
+    {
+        private IHubConnectionContext<dynamic> _clients; 
+        public MessageHandler()
+        {
+            this._clients = GlobalHost.ConnectionManager.GetHubContext<MessageHub>().Clients;
+        }
+
+        public void Handle(PingReceived message)
+        {
+            this._clients.All.pingReceived(message);
+        }
+
+        public void Handle(PingComplete message)
+        {
+            this._clients.All.pingComplete(message);
+        }
+
+        public void Handle(PingFailed message)
+        {
+            this._clients.All.pingFailed(message);
+        }
+    }
+
+    public class MessageHub : Hub
+    {
+         
+    }
+}
