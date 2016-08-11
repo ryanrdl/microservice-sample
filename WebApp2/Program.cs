3e970c70efc54ad426499e840f0600925af27791
ryanrdl <ryan_litvak@jabil.com>
2016-08-11 08:54:35 -0400

    nancyfx + owin + signalr working, still issues though

--- WebApp2/Program.cs
+++ WebApp2/Program.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using Nancy.Hosting.Self;
-
-namespace WebApp2
+﻿namespace WebApp2
 {
+    using System;
+    using Microsoft.Owin.Hosting;
+
     class Program
     {
         static void Main(string[] args)
         {
-            var uri =
-                new Uri("http://localhost:3579");
-
-            using (var host = new NancyHost(uri))
-            {
-                host.Start();
-
+            var uri ="http://localhost:3579";
+             
+            using (WebApp.Start<Startup>(uri))
+            { 
                 Console.WriteLine("Your application is running on " + uri);
                 Console.WriteLine("Press any [Enter] to close the host.");
                 Console.ReadLine();
---
+++ WebApp2/Startup.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Owin;
+using WebApp2;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace WebApp2
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+            app.MapSignalR();
+        }
+    }
+}
