d0aa005a55b150ef4a7f6b8193df2b4117bb96d6
ryanrdl <ryan_litvak@jabil.com>
2016-08-11 08:37:32 -0400

    self hosted web app now loads, still has issues

---
+++ WebApp2/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace WebApp2.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => this.View["index.cshtml"];
+        }
+    }
+}
---
+++ WebApp2/WebClientConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp
+{
+    using SharedKernel;
+
+    public class WebClientConfiguration
+    {
+        public static string PingUrl { get; } = Env.Get($"{nameof(PingUrl)}");
+        public static string PongUrl { get; } = Env.Get($"{nameof(PongUrl)}");
+        public static string RabbitMQConnectionString { get; } = Env.Get(nameof(RabbitMQConnectionString));
+        public static string NServiceBusLicensePath { get; } = Env.Get(nameof(NServiceBusLicensePath));
+        public static string MongoDbConnectionString { get; } = Env.Get(nameof(MongoDbConnectionString));
+    }
+}
